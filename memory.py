c292fd60e7f6f15d1948b04e44c7e84b0d42f2c8
Stelios Papoutsakis <stelios.papoutsakis4@gmail.com>
2023-03-21 14:27:33 -0700

    Added cache impl and comments
    Added cache functionality to memory for reads and writes. Added comments
    for clarity also refactored variable names

--- memory.py
+++ memory.py
@@ -1,18 +1,31 @@
 from enum import Enum
-cycles = 0
 
 class MemoryState( Enum ):
-    READING = 1
-    WRITING = 2
-    IDLE = 3 
-    BUSY = 4 
+    IDLE = 1 
+    BUSY = 2 
+
 class Users( Enum ): 
     USER1 = 1
 
 class Memory():
+    """
+    Used to simulate cache or RAM
+    
+    num_lines : int
+        number of lines this memory holds
+    line_length : int
+        length of each line in words 
+    word_length : int
+        length of each word in bytes
+    response_cycles : int
+        number of cycles it takes for this memory to respond to read/write
+    next_layer : Memory
+        a reference to the next level of memory in the hierachy. 
+        If this is None, then its assumed memory is RAM
+    """
     def __init__( 
             self, 
-            num_words, 
+            num_lines, 
             line_length=4, 
             word_length=4,
             reponse_cycles=1,
@@ -20,70 +33,170 @@ class Memory():
         ):
         
         self.mem = []
-        self.access_cycle = None 
+
+        # info used during access 
+        self.access_counter = 0
         self.access_stage = None
-        self.response_cycles = reponse_cycles        
+        self.access_address = None
+        self.response_cycles = reponse_cycles       
         self.state = MemoryState.IDLE
         self.next_layer = next_layer
+        self.line_length = line_length
         
         # init memory to all 0
-        for _ in range( num_words ):
+        for _ in range( num_lines ):
             line = []
             for _ in range( line_length ):
                 line.append( b'x\00' * word_length )
             self.mem.append( line )
 
-    def read( self, index, user ):
-        if self.state == MemoryState.IDLE:
-            self.access_cycle = cycles
-            self.access_stage = user
-            self.state = MemoryState.READING
-            return self.state
+        # if this is true
+        # we are cache, create valid array and tag array
+        if next_layer is not None:
+            self.valid =  [ False ] * num_lines 
+            self.tag = [ 0 ] * num_lines 
 
-        elif Users.USER1 == self.access_stage and self.state == MemoryState.READING:
-            if cycles >= self.response_cycles + self.access_cycle:
+    def _calc_index_and_tag( self ):
+        '''
+        Internal method used to compute tag and index integer values for cache
+        '''
+        # len( bin( self.line_length - 1 ) ) gets offset bits
+        # -2 to get rid of 0b chars 
+        index_mask_shift = len( bin( self.line_length - 1 ) ) - 2
+        index_mask = len( bin( len( self.mem ) - 1  ) ) - 2 << index_mask_shift
+        index = self.address & index_mask 
+
+        # get rid of index and offset
+        tag_mask = ( 2**16 ) - 1 ^ ( 2**index_mask_shift - 1 + index_mask )
+        tag = self.address & tag_mask
+
+        return tag, index
+
+    def read( self, address, stage ):
+        '''
+        Reads a line that includes the addressed word
+        
+        Parameters
+        ----------
+        address : int
+            2 byte address of word to be read. passed in as integer
+        stage : Users
+            stage of pipline which is accessing the memory
+
+        Returns
+        --------
+        list or MemoryState
+            Returns MemoryState.busy during read execution and list of bytes when finished
+
+        Exceptions
+        ----------
+        NotImplementedError
+            raised if address is larger than amount of words
+        '''
+
+        if self.state == MemoryState.IDLE:
+            if address > len( self.mem ) * self.line_length:
+                raise NotImplementedError
+            self.access_counter = self.response_cycles - 1
+            self.access_stage = stage
+            self.address = address
+            self.state = MemoryState.BUSY
+
+        if stage == self.access_stage and  address == self.address:
+            if self.access_counter < 1:
                 # if the next layer is not none, than this is cache
                 # check to see if we have it
                 if self.next_layer is not None:
-                   pass  
+                    tag, index = self._calc_index_and_tag()
+
+                    # check if hit
+                    if self.valid[ index ] and self.tag[ index ] == tag:
+                        # hit
+                        self.state = MemoryState.IDLE
+                        return self.mem[ index ]
+                    
+                    # else miss
+                    val = self.next_layer.read( address, self.access_stage )
+                    
+                    # update cache
+                    if val != MemoryState.BUSY:
+                        self.valid[ index ] = True
+                        self.tag[ index ] = tag
+                        self.mem[ index ] = val
+                    
+                    self.state = MemoryState.IDLE
+                    return val
 
                 else:
                     # else we are in ram, so get the value
                     self.state = MemoryState.IDLE
-                    return self.mem[ index // 4 ][ index % 4 ]
+                    return self.mem[ address // self.line_length ]
             else:
-                return self.state
-        else:
-            # the memory is busy
-            return MemoryState.BUSY
+                self.access_counter -= 1
+        return self.state
         
         
-    def write( self, index, value, user ):
-        if self.state == MemoryState.IDLE:
-            self.access_cycle = cycles
-            self.access_stage = user
-            self.state = MemoryState.WRITING
-            return MemoryState.WRITING
+    def write( self, address, value, stage ):
+        '''
+        write a word that at specified address
+        
+        Parameters
+        ----------
+        address : bytes
+            2 byte address of word to be written to. passed in as integer
 
-        elif Users.USER1 == self.access_stage and self.state == MemoryState.WRITING:
-            if cycles >= self.response_cycles + self.access_cycle:
+        value : bytes
+            4 byte value to be stored in memory
+        stage : Users
+            stage of pipline which is accessing the memory
+
+        Returns
+        --------
+        MemoryState
+            Returns MemoryState.busy during read execution and MemoryState.idle when write is complete
+        Exceptions
+        ----------
+        NotImplementedError
+            raised if address is larger than amount of words
+
+        '''
+
+        if self.state == MemoryState.IDLE:
+            if address > len( self.mem ) * self.line_length:
+                raise NotImplementedError
+            # sub 1 in order for correct cycles 
+            self.access_counter = self.response_cycles - 1
+            self.access_stage = stage
+            self.address = address
+            self.state = MemoryState.BUSY
+
+        if stage == self.access_stage and address == self.address:
+            if self.access_counter < 1:
                 # if the next layer is not none, than this is cache
                 # we want to write to ram so call write to next layer
+                
                 if self.next_layer is not None:
-                    self.next_layer.write( index, value, user )
+                    # first invalid cache if it exists
+                    _, index = self._calc_index_and_tag()
+                    self.valid[ index ] = False 
+                    return self.next_layer.write( address, value, stage )
+                
                 else:
                     # else we are in ram, so get the value
-                    self.mem[ index // 4 ][ index % 4 ] = value
+                    self.mem[ address // 4 ][ address % 4 ] = value
                     self.state = MemoryState.IDLE
                     return self.state
             else:
-                return self.state
-        else:
-            return MemoryState.BUSY
+                self.access_counter -= 1
+        
+        return self.state
 
 if __name__ == '__main__':
 
-    m = Memory( 1000, reponse_cycles=5 )
+    cycles = 1
+    m = Memory( 1000, reponse_cycles=3 )
+    c = Memory( 16, reponse_cycles=0, next_layer=m )
+
     current_action = None
     while True:
         action = input()
@@ -95,22 +208,22 @@ if __name__ == '__main__':
 
         if current_action is not None:    
             if current_action[ 0 ] == 'READ':
-                read_val = m.read( int( current_action[ 1 ] ),  Users.USER1 )
+                read_val = c.read( int( current_action[ 1 ] ),  Users.USER1 )
                 
             elif current_action[ 0 ] == 'WRITE':
-                write_val = m.write( int( current_action[ 1 ] ) , int( current_action[ 2 ] ).to_bytes( 4, 'big' ), Users.USER1 )
+                write_val = c.write( int( current_action[ 1 ] ) , int( current_action[ 2 ] ).to_bytes( 4, 'big' ), Users.USER1 )
 
         print('-'*20 + 'Cycle {}'.format( cycles ) + '-'*20 )
        
         if current_action and current_action[ 0 ] == 'READ':
             print('READ val {}'.format( read_val ) )
             
-            if read_val != MemoryState.READING:
+            if read_val != MemoryState.BUSY:
                 current_action = None
 
         elif current_action:
             print('WRITE val {}'.format( write_val ) )
-            if write_val != MemoryState.WRITING:
+            if write_val != MemoryState.BUSY:
                 current_action = None
         
         cycles += 1 
