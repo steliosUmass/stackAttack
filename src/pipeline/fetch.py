1ba828ce4c9d38d1db75ecf444e9ccb0d6d0467e
shankars99 <shankars0899@gmail.com>
2023-04-01 11:06:56 -0400

    restructured pipeline/ to src/pipeline

--- src/pipeline/fetch.py
+++ src/pipeline/fetch.py
@@ -26,35 +26,3 @@ class Fetch:
             return load_data_pc_offset
 
         return 48
-
-
-def write(clock, pc, write_val):
-    decode = Decode()
-    for i in range(5):
-        print("clock: ", clock + i, "op:",
-              decode.decode(48), "write val: ", write_val)
-        registers.MEMORY.write(
-            pc, int(write_val).to_bytes(4, 'big'), Users.FETCH)
-
-
-def runner():
-    clock = 0
-    registers.PC = 2
-    registers.INSTR_OFFSET = 0
-    fetch = Fetch()
-    decode = Decode()
-    decode_status = StageState.STALL
-    max_clock = 15
-    write_val = 168565008
-    while clock < max_clock:
-        clock += 1
-        if clock == 5:
-            decode_status = StageState.IDLE
-            write(clock, registers.PC, write_val)
-            clock += 5
-
-        op = fetch.fetch(registers.PC, registers.INSTR_OFFSET, decode_status)
-        print("clock:", clock, "op:", decode.decode(op))
-
-
-runner()
