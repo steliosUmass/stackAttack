d7977762c56ac8b6e4d5bbf7d6fe88cb0211bba3
steliosUmass <spapoutsakis@umass.edu>
2023-04-02 20:14:26 -0400

    Added functionilty to load button which loads program

--- src/gui/main.py
+++ src/gui/main.py
@@ -2,6 +2,8 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication
+from tkinter import filedialog
+import tkinter 
 import view_models
 import sim_gui
 import sys
@@ -29,19 +31,49 @@ class Simulator(QtWidgets.QMainWindow, sim_gui.Ui_MainWindow):
 
         # init register table
         self.regView.setModel( view_models.RegisterModel( registers.PC, registers.INSTR_OFFSET, registers.LINK, registers.PUSH, registers.POP ) )
-            
+
+        # add listener to load program
+        self.LoadButton.clicked.connect( self.load_program )           
+    
     def index_changed_memCombo( self, index ):
         if index == 0:
-            # show ram ( only first 200 lines for now )
+            # show ram
             self.memTable.setModel( view_models.RamModel( registers.MEMORY.next_layer.mem ) )
         elif index == 1:
             # show cache
             self.memTable.setModel( view_models.CacheModel( registers.MEMORY.tag, registers.MEMORY.mem, registers.MEMORY.valid ) )
 
+    def load_program( self ):
+        initial_dir = os.path.dirname( os.path.dirname( os.path.dirname(  os.path.realpath( __file__ ) ) ) )
+        filename = filedialog.askopenfilename( initialdir = initial_dir, title = "Select Program" )
+        b = None
+        with open( filename, 'rb' ) as f:
+            b = bytearray( f.read() )
+       
+        mem_index = 0
+        for i in range( 0, len( b ), 4 ):
+            val = None
+            if i + 4 > len(b):
+                val = bytearray( b[ i: ] )
+                pad_num = 4 - ( len( b ) - i ) 
+                for _ in range( pad_num ):
+                    val.append( 0 ) 
+                val = bytes( val )
+            else:
+                val = bytes( b[ i: i + 4 ] )
+           
+            # put value in RAM
+            registers.MEMORY.next_layer.mem[ mem_index // 4 ][ mem_index % 4 ] = val
+            mem_index += 1
+
+        # refresh gui with program 
+        self.index_changed_memCombo( self.memCombo.currentIndex() )
 
 def main():
     app = QApplication(sys.argv)
     sim = Simulator()
+    root = tkinter.Tk()
+    root.withdraw()
     sim.show()
     app.exec_()
 
