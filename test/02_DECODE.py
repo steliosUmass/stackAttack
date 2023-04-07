052bbc4300c6d2dd60d9ef44b96234aa20a1618c
shankars99 <shankars0899@gmail.com>
2023-04-07 12:18:54 -0400

    testing the instruction types

--- test/02_DECODE.py
+++ test/02_DECODE.py
@@ -12,34 +12,57 @@ except ImportError:
 
 
 class TestDecode(unittest.TestCase):
+    def __init__(self, methodName: str = "runTest") -> None:
+        super().__init__(methodName)
+        self.decode = Decode().decode
+
+    def create_instruction(self, type=0, op=48, operand_1=None, operand_2=None, operand_3=None, is_alu=False, is_branch=False, is_mem_access=False, squash=False):
+        return {
+            'type': type,
+            'Op': op,
+            'Operand_1': operand_1,
+            'Operand_2': operand_2,
+            'Operand_3': operand_3,
+            'is_alu': is_alu,
+            'is_branch': is_branch,
+            'is_mem_access': is_mem_access,
+            'squash': squash
+        }
+
     def test_DECODE_NOOP(self):
-        decode = Decode()
-        curr_instr = decode.decode(Op.NOOP)
-        expected_instr = {'Op': Op.NOOP, 'Operand_1': None, 'Operand_2': None,
-                          'Operand_3': None, 'is_alu': False, 'is_branch': False, 'is_mem_access': False}
+        curr_instr = self.decode(48)
+        expected_instr = self.create_instruction(op=Op.NOOP)
         self.assertEqual(curr_instr, expected_instr), "Decode test failed"
 
     def test_DECODE_STACK_PUSH_POP(self):
-        decode = Decode()
         stack_size = registers.STACK.top_index
         registers.STACK.push(23)
         registers.STACK.push(47)
         self.assertEqual(registers.STACK.top_index,
                          stack_size + 2), "Stack push failed"
         stack_size = registers.STACK.top_index
-        decode.decode(Op.ADD)
+        self.decode(16)
         self.assertEqual(registers.STACK.top_index,
                          stack_size - 2), "Stack pop failed"
 
-    def test_DECODE_POP_2(self):
-        decode = Decode()
+    def test_DECODE_POP_2_ADD(self):
         registers.STACK.push(1)
         registers.STACK.push(2)
-        test_instr = decode.decode(Op.ADD)
-        expected_instr = {'Op': Op.ADD, 'Operand_1': 2, 'Operand_2': 1,
-                          'Operand_3': None, 'is_alu': True, 'is_branch': False, 'is_mem_access': False}
+        test_instr = self.decode(16)
+        expected_instr = self.create_instruction(
+            op=Op.ADD, operand_1=2, operand_2=1, is_alu=True)
         self.assertEqual(
             test_instr, expected_instr), "Decode test for ADD failed"
+
+    def test_DECODE_PUSH_VAL(self):
+        # val is made up of 8 bits (left to right)
+        # first bit is 1 - the type indicator,
+        # next 2 bits are 00 - the op code for PUSH_VAL,
+        # and the last 5 bits are 00011 - the operand
+        curr_instr = self.decode(int(b'10000011', 2))
+        expected_instr = self.create_instruction(
+            type=1, op=Op.PUSH_VAL, operand_1=int(b'00011', 2))
+        self.assertEqual(curr_instr, expected_instr), "Decode test failed"
 
 
 if __name__ == '__main__':
