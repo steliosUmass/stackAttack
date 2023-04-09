bbe7ec3732c117565fd9eb528a571ebb12419fa6
shankars99 <shankars0899@gmail.com>
2023-04-09 14:26:28 -0400

    updated fetch to the new document. passes tests.

--- src/pipeline/fetch.py
+++ src/pipeline/fetch.py
@@ -1,8 +1,6 @@
 from memory import MemoryState, Users
 from stage_state import StageState
 import registers
-from decode import Decode
-
 
 class Fetch:
     # TODO: Add support for instruction offset
@@ -27,25 +25,25 @@ class Fetch:
             # Get the PC and the instruction offset
             pc = registers.PC
             offset = registers.INSTR_OFFSET
-    
+
             self.load_data = self.load(pc)
             # Check if the memory or the decode stage is busy
             if self.load_data != MemoryState.BUSY and decode_state != StageState.STALL:
                 # Set the fetch status to idle
                 self.status = StageState.IDLE
-    
+
                 # Update the PC and the instruction offset
                 registers.INSTR_OFFSET = offset + 1
                 registers.PC = pc + registers.INSTR_OFFSET // self.instr_per_word
                 registers.INSTR_OFFSET = registers.INSTR_OFFSET % self.instr_per_word
-    
+
                 # Extract the instruction from the load data
                 load_data_pc = self.load_data[pc % 4]
                 load_data_pc_offset = load_data_pc[offset]
                 return load_data_pc_offset
-    
+
         # If the memory or the decode stage is busy, return Op.NOOP
-        return 45 + 2**7
+        return (48 + (2<<6))
 
     def fetch_back_pass(self, decode_status):
         if decode_status['status'] == StageState.STALL:
@@ -53,4 +51,4 @@ class Fetch:
         return decode_status
 
     def fetch_forward_pass(self, decode_status, should_issue):
-        return self.fetch(decode_state=decode_status['status'], should_issue)
+        return self.fetch(decode_state=decode_status['status'], should_issue=should_issue)
